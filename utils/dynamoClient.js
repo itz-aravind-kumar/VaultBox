51a49425169258b7572d60da5f36f93556f87152
Muddana Aravind Kumar <aravindk.muddana@gmail.com>
2025-06-24 23:41:58 +0530

    Update dynamoClient.js

--- utils/dynamoClient.js
+++ utils/dynamoClient.js
@@ -2,6 +2,6 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 
 const client = new DynamoDBClient({ region: process.env.AWS_REGION });
-const ddb = DynamoDBDocumentClient.from(client);
+const ddb =DynamoDBDocumentClient.from(client);
 
 module.exports = { ddb };
